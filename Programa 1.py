28275d0c1ead3ea880c5947a34320c829ac6a8eb
Agustín <alaraf85@gmail.com>
2024-03-06 01:32:42 -0600

    Cambios en el Branch

---
+++ Programa 1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print ('Bienvenido al Curso de Phyton')
+
+
+# In[5]:
+
+
+print (123)
+
+
+# In[6]:
+
+
+x=10.5
+y=20
+
+
+# In[8]:
+
+
+print(x)
+
+
+# In[10]:
+
+
+print("El valor de la variable x es",x)
+
+
+# In[11]:
+
+
+z= x+y
+print("El resultado de z es",z)
+
+
+# In[12]:
+
+
+long_objeto1=10.5
+long_objeto2=20
+long_objeto3= long_objeto1+long_objeto2
+print(long_objeto3)
+
+
+# In[ ]:
+
+<<<<<<< HEAD
+## Se agrega cambio
+a= 15
+b= 56
+d=a*b
+
+
+=======
+>>>>>>> 38bab02a161241c52e1a8551654b75c1bf5625bb
+
+
+
